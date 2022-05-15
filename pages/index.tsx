a2833bbc19d766a6b4728f4374c1fc08caf089ec
ahonn <ahonn95@outlook.com>
2022-05-15 12:01:17 +0800

    feat: padding zero in sm screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,27 +5,33 @@ import Image from 'next/image';
 import dynamic from 'next/dynamic';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import {
-    cityState,
   clubTypesState,
   frisbeeClubsState,
   provinceState,
 } from '../store/state';
 import { useEffect } from 'react';
 import { ClubType } from '../components/common/club-type';
+import { useMediaQuery } from '@mantine/hooks';
 
 const FrisbeeMap = dynamic(() => import('../components/FrisbeeMap'), {
   ssr: false,
   loading: () => <Skeleton className="w-full h-1/2screen min-h-96 shadow" />,
 });
-const ProvinceBarChart = dynamic(() => import('../components/ProvinceBarChart'), {
-  loading: () => <Skeleton className="my-4 w-full h-96 shadow" />,
-});
+const ProvinceBarChart = dynamic(
+  () => import('../components/ProvinceBarChart'),
+  {
+    loading: () => <Skeleton className="my-4 w-full h-96 shadow" />,
+  },
+);
 const CityBarChart = dynamic(() => import('../components/CityBarChart'), {
   loading: () => <Skeleton className="my-4 w-full h-96 shadow" />,
 });
-const EstablishmentLineChart = dynamic(() => import('../components/EstablishmentLineChart'), {
-  loading: () => <Skeleton className="my-4 w-full h-96 shadow" />,
-});
+const EstablishmentLineChart = dynamic(
+  () => import('../components/EstablishmentLineChart'),
+  {
+    loading: () => <Skeleton className="my-4 w-full h-96 shadow" />,
+  },
+);
 
 export async function getStaticProps() {
   const database = VikaFrisbeeDatabse.getInstance();
@@ -57,6 +63,7 @@ const Home: NextPage<IHomeProps> = (props) => {
 
   return (
     <AppShell
+      padding={0}
       header={
         <Header height={60}>
           <Container size="xl" py={5}>
@@ -66,20 +73,24 @@ const Home: NextPage<IHomeProps> = (props) => {
       }
     >
       <Container size="xl" px={0}>
-        <FrisbeeMap className="w-full h-1/2screen min-h-96 z-0 shadow" />
-        <h2 className="text-4xl mt-8 font-serif">
-          飞盘地图数据统计{province ? `(${province})` : ''}
-        </h2>
-        {province === '' ? (
-          <>
-            <ProvinceBarChart />
-            <EstablishmentLineChart />
-          </>
-        ) : (
-          <>
-            <CityBarChart province={province} />
-          </>
-        )}
+        <div className="px-0 pb-4 sm:p-4">
+          <FrisbeeMap className="w-full h-1/2screen min-h-96 z-0 shadow" />
+        </div>
+        <div className="px-4">
+          <h2 className="text-4xl mt-8 font-serif">
+            飞盘地图数据统计{province ? `(${province})` : ''}
+          </h2>
+          {province === '' ? (
+            <>
+              <ProvinceBarChart />
+              <EstablishmentLineChart />
+            </>
+          ) : (
+            <>
+              <CityBarChart province={province} />
+            </>
+          )}
+        </div>
       </Container>
     </AppShell>
   );
